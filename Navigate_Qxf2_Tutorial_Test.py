6cfa51afed9ecf6f811a6b2d5d935f0d478741f5
Qxf2Circle <testQxf2@gmail.com>
2015-10-14 11:26:56 +0530

    Update Navigate_Qxf2_Tutorial_Test.py

--- Navigate_Qxf2_Tutorial_Test.py
+++ Navigate_Qxf2_Tutorial_Test.py
@@ -9,11 +9,6 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 # The driver.get method will navigate to a page given by the URL
 driver.get("http://localhost/selenium-tutorial-main.html")
-# Create a screenshots directory if not present
-if not (os.path.exists('./tests/screenshots')):
-    os.makedirs('./tests/screenshots')
-# Save screenshot in the created directory
-driver.save_screenshot('./tests/screenshots/Qxf2_Tutorial_page.png')
 # Assert the Page Title
 assert "Qxf2 Services: Selenium training main" in driver.title
 # Close the browser window
