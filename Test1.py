6fe7d81318d38ec550ff5a4f4a607d17a9c63281
avinashshetty <avinash@qxf2.com>
2016-02-05 12:07:29 +0530

    Adding other test

---
+++ Test1.py
@@ -0,0 +1,13 @@
+"""
+Selenium Test to login to Qxf2 Tutorial Page and assert the title
+"""
+from selenium import webdriver
+
+# Create an instance of Firefox WebDriver
+driver = webdriver.Firefox()
+# The driver.get method will navigate to a page given by the URL
+driver.get("http://localhost/selenium-tutorial-main.html")
+# Assert the Page Title
+assert "Qxf2 Services: Selenium training main" in driver.title
+# Close the browser window
+driver.close()
